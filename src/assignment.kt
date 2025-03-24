f007e597b75fddba5ea8f929e2baf057414d472a
michaeltekri <michaeltekri84@gmail.com>
2025-03-24 13:44:09 +0300

    initial commit

--- src/assignment.kt
+++ src/assignment.kt
@@ -1,8 +1,31 @@
 import java.util.Scanner
 
 fun main() {
-    var read=Scanner(System.`in`)
+
+    var read = Scanner(System.`in`)
     println("Enter number:")
-    var number=read.nextInt()
+
+    var d= read.nextInt()
+    println("$d is first number")
+    var a= read.nextInt()
+    println("$a is first number")
+    var c= read.nextInt()
+    println("$c is first number")
+
+    var max= listOf(d,a,c).max()
+    println("$max is greatest number")
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
